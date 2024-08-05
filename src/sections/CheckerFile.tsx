54da03a066291c5b4ec5a54ddad6c50ee3a75098
Octavio Martinez <omartinez1618@gmail.com>
2024-08-05 17:32:52 -0400

    markup

--- src/sections/CheckerFile.tsx
+++ src/sections/CheckerFile.tsx
@@ -2,10 +2,11 @@
 import { useEffect, useState } from "react"
 import { Editor } from "@monaco-editor/react"
 import { experimental_useObject as useObject } from "ai/react"
+import { parse } from "marked"
 import { checkSchema } from "@/app/api/file-check/schema"
-import { CODE, NOT_RATE, ISSUES } from "@/tools/constants"
 import HeaderEditor from "@/components/HeaderEditor"
 import NavigationDirectory from "@/components/NavigationDirectory"
+import { CODE, NOT_RATE, ISSUES } from "@/tools/constants"
 
 
 export default function CheckerFile({ user, repo, files }: { user:string, repo:string, files: FileType[] }) {
@@ -13,11 +14,12 @@ export default function CheckerFile({ user, repo, files }: { user:string, repo:s
         api: "/api/file-check",
         schema: checkSchema
     })
+    const [marked, setMarked] = useState("")
     const [tab, setTab] = useState(CODE)
     const [loading, setLoading] = useState(false)
     const [content, setContent] = useState<ContentType>({
         value: "",
-        desecription: "",
+        description: "",
         fixes: [],
         rate: NOT_RATE
     })
@@ -72,6 +74,10 @@ export default function CheckerFile({ user, repo, files }: { user:string, repo:s
         submit({ file: path, content: content.value })
     }
 
+    const getMarked = (text:string) => {
+        return parse(text, {  gfm:true, breaks:true  } ) as string
+    }
+
 
     return <section className="w-full grid grid-cols-12 gap-0 shadow-lg shadow-slate-800 border border-black rounded-lg overflow-hidden divide-x divide-black">
         <nav className="col-span-3 bg-slate-200">
@@ -91,12 +97,10 @@ export default function CheckerFile({ user, repo, files }: { user:string, repo:s
             { tab===CODE && <Editor
                 defaultLanguage="typescript"
                 defaultValue=""
+                onChange={(value) => false }
                 loading={loading}
                 value={content.value}/>}
-            { tab===ISSUES && <Editor
-                defaultLanguage="html"
-                defaultValue=""
-                value={`## Descripcion \n${content?.description} \n\n## Posibles Mejoras \n${content?.fixes?.join('\n')} `}/>}
+            { tab===ISSUES && <div id="preview-markup" className="p-4" dangerouslySetInnerHTML={{ __html: getMarked( `### Descripcion \n${content?.description} \n\n### Posibles Mejoras \n${content?.fixes?.join('\n')} ` ) }} />}
 
         </div>
     </section>
